470cd0c8d0fece128fbf73ba226d87bf945a654d
pyunghun <564564q@naver.com>
2025-01-15 22:35:00 +0900

    your commit message

---
+++ src/screens/modal/foodModal/FreeFoodModal.js
@@ -0,0 +1,278 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Modal,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useDispatch, useSelector } from 'react-redux'; // useDispatch 가져오기
+import { saveTotalFoodData } from '../../../apis/FoodApi';
+
+const FreeFoodModal = ({ isVisible, onClose, setFreeOnclose, memberId, mealType }) => {
+    
+    const dispatch = useDispatch();
+
+    const [unit, setUnit] = useState('g');
+    const [calories, setCalories] = useState('');
+    const [carbs, setCarbs] = useState('');
+    const [protein, setProtein] = useState('');
+    const [fat, setFat] = useState('');
+    const [quantity, setQuantity] = useState('');
+
+    useEffect(() => {
+        const fetchUnits = async () => {
+            try {
+                const gOrOzUnit = await AsyncStorage.getItem('gOrOzUnit');
+                if (gOrOzUnit === null) {
+                    await AsyncStorage.setItem('gOrOzUnit', 'g');
+                    setUnit('g');
+                } else {
+                    setUnit(gOrOzUnit);
+                }
+            } catch (error) {
+                console.error('Error fetching units:', error);
+            }
+        };
+
+        fetchUnits();
+    }, []);
+
+    const handleUnitChange = async (newUnit) => {
+        if (newUnit === unit) return;
+
+        const conversionFactor = newUnit === 'oz' ? 1 / 28.3495 : 28.3495; // g ↔ oz 변환 계수
+
+        // 단위가 변경되면 모든 입력값을 변환
+        setQuantity(convertUnits(quantity, newUnit));
+        setCarbs(convertUnits(carbs, newUnit));
+        setProtein(convertUnits(protein, newUnit));
+        setFat(convertUnits(fat, newUnit));
+
+        setUnit(newUnit);
+        await AsyncStorage.setItem('gOrOzUnit', newUnit);
+    };
+
+    const convertUnits = (value, toUnit) => {
+        if (!value) return '';
+        const numericValue = parseFloat(value);
+        if (isNaN(numericValue)) return '';
+        return toUnit === 'oz' ? (numericValue / 28.3495).toFixed(1) : (numericValue * 28.3495).toFixed(1);
+    };
+
+    // 유효성 검사 함수: 숫자와 소수점만 허용
+    const handleNumericInput = (text) => {
+        let numericValue = text
+            .replace(/[^0-9.]/g, '')  // 숫자와 소수점만 허용
+            .replace(/(\..*?)\..*/g, '$1')  // 소수점 하나만 허용
+            .replace(/^0+(?=\d)/, '');  // 숫자 앞의 불필요한 0 제거
+    
+        // 숫자가 99999를 초과하면 99999로 제한
+        const parsedValue = parseFloat(numericValue);
+        return !isNaN(parsedValue) && parsedValue > 99999 ? '99999' : numericValue;
+    };
+    
+
+
+    const handleSubmit = async () => {
+        const recipeNames = ["임시식단"];
+    
+        // 소수점 1자리로 반올림하는 함수
+        const roundToOneDecimal = (value) => {
+            return Math.round(value * 10) / 10;
+        };
+    
+        // 단위에 따른 변환 처리
+        const totalNutrition = {
+            grams: unit === 'oz'
+                ? roundToOneDecimal(parseFloat(quantity) * 28.3495 || 0) // oz → g 변환 후 반올림
+                : roundToOneDecimal(parseFloat(quantity) || 0),  // g이면 그대로 사용 후 반올림
+            kcal: roundToOneDecimal(parseFloat(calories) || 0), // 칼로리 반올림
+            protein: unit === 'oz'
+                ? roundToOneDecimal(parseFloat(protein) * 28.3495 || 0) // oz → g 변환 후 반올림
+                : roundToOneDecimal(parseFloat(protein) || 0),  // g이면 그대로 사용 후 반올림
+            carbs: unit === 'oz'
+                ? roundToOneDecimal(parseFloat(carbs) * 28.3495 || 0) // oz → g 변환 후 반올림
+                : roundToOneDecimal(parseFloat(carbs) || 0),  // g이면 그대로 사용 후 반올림
+            fat: unit === 'oz'
+                ? roundToOneDecimal(parseFloat(fat) * 28.3495 || 0) // oz → g 변환 후 반올림
+                : roundToOneDecimal(parseFloat(fat) || 0),  // g이면 그대로 사용 후 반올림
+        };
+
+        const today = new Date();
+    
+        // 연, 월, 일 값을 추출하고 두 자리로 포맷
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 1을 더해야 합니다.
+        const day = String(today.getDate()).padStart(2, '0'); // 날짜를 두 자리로 포맷
+
+        // 원하는 형식 (YYYY-MM-DD)
+        const formattedDate = `${year}-${month}-${day}`;
+    
+
+        try {
+            await saveTotalFoodData(memberId, mealType, formattedDate, totalNutrition, recipeNames, null, dispatch);
+            // 데이터 저장 성공 시 모달 닫기
+        } catch (error) {
+            console.error('데이터 저장 실패:', error);
+        }
+
+        setFreeOnclose(true);
+        onClose(); // 성공 시 모달 닫기
+    };
+    
+
+    return (
+        <Modal transparent={true} visible={isVisible} animationType="fade">
+            <View style={styles.overlay}>
+                <View style={styles.modalContainer}>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>영양 정보 입력</Text>
+                        <TouchableOpacity onPress={onClose}>
+                            <Ionicons name="close" size={24} color="#999" style={styles.closeIcon} />
+                        </TouchableOpacity>
+                    </View>
+
+                    <ScrollView style={styles.content}>
+                        <Text style={{fontSize: 15, fontWeight:'bold', marginBottom: 5}}>단위선택</Text>
+                        <View style={styles.unitSelection}>
+                            <TouchableOpacity
+                                style={unit === 'g' ? styles.selectedUnit : styles.unitButton}
+                                onPress={() => handleUnitChange('g')}
+                            >
+                                <Text style={styles.unitText}>g</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={unit === 'oz' ? styles.selectedUnit : styles.unitButton}
+                                onPress={() => handleUnitChange('oz')}
+                            >
+                                <Text style={styles.unitText}>oz</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={{fontSize: 15, fontWeight:'bold', marginBottom: 5}}>임시 영양성분 입력</Text>
+
+                        <TextInput
+                            style={styles.input}
+                            placeholder="용량"
+                            value={quantity}
+                            onChangeText={(text) => setQuantity(handleNumericInput(text))}
+                            keyboardType="numeric"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="칼로리"
+                            value={calories}
+                            onChangeText={(text) => setCalories(handleNumericInput(text))}
+                            keyboardType="numeric"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="탄수화물"
+                            value={carbs}
+                            onChangeText={(text) => setCarbs(handleNumericInput(text))}
+                            keyboardType="numeric"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="단백질"
+                            value={protein}
+                            onChangeText={(text) => setProtein(handleNumericInput(text))}
+                            keyboardType="numeric"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="지방"
+                            value={fat}
+                            onChangeText={(text) => setFat(handleNumericInput(text))}
+                            keyboardType="numeric"
+                        />
+
+                    </ScrollView>
+
+                    <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+                        <Text style={styles.submitButtonText}>완료</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContainer: {
+        width: '80%',
+        minHeight: 10,
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        padding: 15,
+        justifyContent: 'space-between',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    closeIcon: {
+        marginRight: 10,
+    },
+    content: {
+        marginTop: 20,
+    },
+    unitSelection: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        gap: 10,
+        marginBottom: 20
+    },
+    unitButton: {
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: '#D4D4D8',
+        flex: 1,
+    },
+    selectedUnit: {
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: '#A29BFE',
+        flex: 1,
+    },
+    unitText: {
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#CCC',
+        borderRadius: 8,
+        padding: 10,
+        marginBottom: 10,
+    },
+    submitButton: {
+        backgroundColor: '#497CF4',
+        padding: 12,
+        borderRadius: 8,
+        alignItems: 'center',
+        marginTop: 30
+    },
+    submitButtonText: {
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
+
+export default FreeFoodModal;
--- src/screens/record/Record.js
+++ src/screens/record/Record.js
@@ -1,29 +1,85 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import Footer from '../common/Footer'; 
-import Header from '../common/Header'; // 커스텀 헤더 컴포넌트 임포트
+import Header from '../common/Header';
+import { Calendar } from 'react-native-calendars';
 
 const Record = ({ navigation }) => {
+    const [markedDates, setMarkedDates] = useState({
+        '2025-01-15': {
+            dots: [
+                { key: 'exercise', color: '#00adf5' }, // 운동 관련 마커
+                { key: 'food', color: '#FF76CF' }, // 식단 관련 마커
+                { key: 'body', color: '#FAF335' }, // 신체 관련 마커
+            ],
+        },
+    });
 
-    useEffect(() => {
-        console.log("=====================기록 페이지 ========================")
-    }, []);
+    const [theme, setTheme] = useState({
+        backgroundColor: '#1A1C22', 
+        calendarBackground: '#222732',
+        dayTextColor: '#ffffff',
+        monthTextColor: '#ffffff',
+        textDisabledColor: '#5d616c',
+        todayTextColor: '#16D548',
+        arrowColor: '#ffffff',
+        selectedDayBackgroundColor: '#00adf5',
+        selectedDayTextColor: '#ffffff',
+        textDayFontWeight: 'bold',
+        textMonthFontWeight: 'bold',
+        textDayHeaderFontWeight: 'bold',
+    });
 
+    const handleDayPress = (day) => {
+        const newMarkedDates = {
+            ...markedDates,
+            [day.dateString]: {
+                ...markedDates[day.dateString],
+                selected: true,
+                selectedColor: '#497CF4', // 선택된 날짜 배경색
+                selectedTextColor: '#ffffff', // 선택된 날짜 텍스트 색상
+            },
+        };
+
+        // 이전 선택 해제
+        Object.keys(newMarkedDates).forEach((date) => {
+            if (date !== day.dateString && newMarkedDates[date]?.selected) {
+                delete newMarkedDates[date].selected;
+                delete newMarkedDates[date].selectedColor;
+                delete newMarkedDates[date].selectedTextColor;
+            }
+        });
+
+        setMarkedDates(newMarkedDates);
+        console.log('Selected date:', day.dateString);
+    };
 
     return (
-        <View style={{ flex: 1, justifyContent: 'space-between', backgroundColor: '#1A1C22' }}>
-            {/* 커스텀 헤더 컴포넌트 사용 */}
+        <View style={{ flex: 1, justifyContent: 'space-between', backgroundColor: theme.backgroundColor }}>
             <Header title="Record" navigation={navigation} />
             
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Record Page</Text>
-            </View>
+            <ScrollView style={{ padding: 15 }} keyboardShouldPersistTaps="handled">
+                <Calendar
+                    markedDates={markedDates}
+                    onDayPress={handleDayPress}
+                    style={styles.calendar}
+                    theme={theme}
+                    markingType="multi-dot" // 다중 마커 타입 사용
+                    monthFormat="yyyy-MM" // 월 텍스트 형식 지정
+                />
+            </ScrollView>
 
-            {/* Footer 컴포넌트 사용 */}
             <Footer navigation={navigation} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    calendar: {
+        borderRadius: 15,
+        padding: 5,
+        paddingBottom: 10,
+    },
+});
 
 export default Record;
